8fed19697b3a51e7b3a881d9014a78461cc639d9
Jongsun Park <jongsun250@gmail.com>
2023-12-30 23:18:50 +0000

    tailwindcss bundling issue fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,5 @@ module.exports = {
     "./templates/customers/*.liquid",
     "./templates/*.liquid",
   ],
-  theme: {
-    colors: {
-      secondary_400: "#0468DB",
-      secondary_600: "#004289",
-      background: "#F5F6F4",
-    },
-  },
   plugins: [],
 };
